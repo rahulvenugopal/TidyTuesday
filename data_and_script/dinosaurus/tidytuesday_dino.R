7609db2c9b213dd70826560a70b82cdfb0aa2e2b
Rahul venugopal <rhlvenugopal@gmail.com>
2020-10-21 16:17:08 +0530

    Cleaned code
    Added code for boxplots
    Haven't included this in viz

--- data_and_script/dinosaurus/tidytuesday_dino.R
+++ data_and_script/dinosaurus/tidytuesday_dino.R
@@ -55,3 +55,19 @@ scatters + theme(legend.position = "none") + labs (title = "Lies, damned lies, a
   theme(plot.title = element_text(hjust = 0.5)) +
   violins + theme(legend.position = "none") + labs (title = "Now you see me") +
   theme(plot.title = element_text(hjust = 0.5))
+
+# One can add boxplots also
+boxes <- ggplot(data = data, aes(x = y, y = x, color = dataset)) + 
+  geom_boxplot() +
+  facet_wrap(~ dataset, nrow = 3) +
+  theme(plot.background = element_rect(fill = '#fdfff5')) +
+  theme_minimal() +
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_textbox(
+      size = 8,
+      color = "white", fill = "#5D729D", box.color = "#4A618C",
+      halign = 0.5, linetype = 1, r = unit(5, "pt"), width = unit(1, "npc"),
+      padding = margin(2, 0, 1, 0), margin = margin(3, 3, 3, 3)
+    )
+  )
